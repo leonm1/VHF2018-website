e0aedce1b783f04c7f25e120253338240cb84df2
cktang88 <arctangent20@yahoo.com>
2018-08-13 15:53:20 -0500

    White MLH badge

--- components/MlhBadge.vue
+++ components/MlhBadge.vue
@@ -2,30 +2,30 @@
 <a id="mlh-trust-badge" style="display:block;max-width:100px;min-width:60px;position:fixed;right:50px;top:0;width:10%;z-index:10000"
 	 href="https://mlh.io/seasons/na-2019/events?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2019-season&utm_content=blue"
 	 target="_blank">
-		<img src="https://s3.amazonaws.com/logged-assets/trust-badge/2019/mlh-trust-badge-2019-blue.svg" alt="Major League Hacking 2019 Hackathon Season"
+		<img src="https://s3.amazonaws.com/logged-assets/trust-badge/2019/mlh-trust-badge-2019-white.svg" alt="Major League Hacking 2019 Hackathon Season"
 		 style="width:100%">
 	</a>
 </template>
 
 <style lang="scss">
 #mlh-trust-badge {
-    display: block;
-    min-width: 50px;
-    max-width: 50px;
-    position: absolute;
-    right: 15px;
-    top: 0;
-    width: 10%;
-    z-index: 5;
+  display: block;
+  min-width: 50px;
+  max-width: 50px;
+  position: absolute;
+  right: 15px;
+  top: 0;
+  width: 10%;
+  z-index: 5;
 
-    @media (min-width: 960px) {
-        max-width: 75px;
-        right: 35px;
-    }
+  @media (min-width: 960px) {
+    max-width: 75px;
+    right: 35px;
+  }
 
-    @media (min-width: 1200px) {
-        max-width: 96px;
-        right: 50px;
-    }
+  @media (min-width: 1200px) {
+    max-width: 96px;
+    right: 50px;
+  }
 }
 </style>
