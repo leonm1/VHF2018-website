379e5c1c29c318af498f4fa40721fac88cde4eaf
Matthew Leon <matthew.leon@vanderbilt.edu>
2018-09-23 19:35:14 -0500

    Fix scrolling

--- pages/index.vue
+++ pages/index.vue
@@ -163,6 +163,7 @@ body {
 
 .main {
   max-width: 100%;
+  height: 100vh;
   background: $background;
 
   display: block;
@@ -198,7 +199,7 @@ body {
 }
 
 .landing-section {
-  height: 100vh;
+  min-height: 100vh;
   padding: 30px 18px;
   padding-top: 10vh;
 
